c9ab978c8381c846e9ed05b24c5d758735cc1532
PhanDung520 <57535358+PhanDung520@users.noreply.github.com>
2022-09-30 16:50:06 +0700

    fix1

--- pokemon_app/lib/pages/home_page/tabs/all_pokemon_tab.dart
+++ pokemon_app/lib/pages/home_page/tabs/all_pokemon_tab.dart
@@ -20,36 +20,38 @@ class _AllPokemonTabState extends ConsumerState<AllPokemonTab> {
   @override
   void initState() {
     // TODO: implement initState
-    List<Pokemon> listPoke =[];
-    pokeGet(listPoke);//get data
-    ref.read(pokeGet2Provider.notifier).state = listPoke;
+    pokeGet();//get data
     super.initState();
   }
 
-  Future<void> pokeGet(List<Pokemon> listPoke) async{
-    await firestore.collection('pokemons').get().then((value) => {
-      value.docs.forEach((element) {
-        listPoke.add(Pokemon(element['class'], element['attack'], element['height'], element['hp'], element['id'], element['image'], element['name'], element['speed'], element['weight']));
-      })
-    });
+  Future<void> pokeGet() async{
+    List<Pokemon> listPoke =[];
+    if(mounted){
+      await firestore.collection('pokemons').get().then((value) => {
+        value.docs.forEach((element) {
+          listPoke.add(Pokemon(element['class'], element['attack'], element['height'], element['hp'], element['id'], element['image'], element['name'], element['speed'], element['weight']));
+        })
+      });
+      ref.read(pokeGet2Provider.notifier).state = listPoke;
+    }
   }
   @override
   Widget build(BuildContext context) {
-
+    final data = ref.watch(pokeGet2Provider);
     return Container(
       padding: EdgeInsets.all(15),
-      child: GridView.count(physics: BouncingScrollPhysics(),childAspectRatio: (MediaQuery.of(context).size.width*0.5/ 250),crossAxisCount: 2, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0, children: List.generate(ref.watch(pokeGet2Provider).length, (index) {
+      child: GridView.count(physics: BouncingScrollPhysics(),childAspectRatio: (MediaQuery.of(context).size.width*0.5/ 250),crossAxisCount: 2, crossAxisSpacing: 8.0, mainAxisSpacing: 8.0, children: List.generate(data.length, (index) {
         return InkWell(
           onTap: (){
             //move to details
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage(pokemon: ref.watch(pokeGet2Provider)[index], userId: widget.userId,)));
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage(pokemon: data[index], userId: widget.userId,)));
           },
           child: Container( child: Column(crossAxisAlignment: CrossAxisAlignment.start,mainAxisAlignment: MainAxisAlignment.spaceAround,children: [
-            Container(child: Image.network(ref.watch(pokeGet2Provider)[index].image),height: 100,width: MediaQuery.of(context).size.width*0.6,),
+            Container(child: Image.network(data[index].image),height: 100,width: MediaQuery.of(context).size.width*0.6,),
             Container(margin: EdgeInsets.only(left: 10),child: Column(crossAxisAlignment: CrossAxisAlignment.start,children: [
-              Container(child: Text('#${ref.watch(pokeGet2Provider)[index].pokeId.toString()}'),),
-              Container(child: Text(ref.watch(pokeGet2Provider)[index].name, style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),),),
-              Container(child: Text(ref.watch(pokeGet2Provider)[index].class1, style: TextStyle(color: AppColors.lightTextColor, fontSize: 14),),)
+              Container(child: Text('#${data[index].pokeId.toString()}'),),
+              Container(child: Text(data[index].name, style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),),),
+              Container(child: Text(data[index].class1, style: TextStyle(color: AppColors.lightTextColor, fontSize: 14),),)
             ],),)
           ],),
             decoration: BoxDecoration(
